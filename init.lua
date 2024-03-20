eba475a52cdec0eec9203afe02cb17d4accd29b8
Jacob Christensen <jchristensen888@gmail.com>
2024-03-19 21:02:41 -0600

    fix error detecting os

--- init.lua
+++ init.lua
@@ -109,8 +109,27 @@ vim.cmd[[colorscheme solarized-osaka]]
 -- 
 -- Stuff that doesn't work on Windows
 --
-if not platform == "win" then
+if not (platform == "win") then
 
+    --
+    -- LSP Zero
+    --
+    local lsp_zero = require("lsp-zero");
+    lsp_zero.on_attach(function(client, bufnr)
+        lsp_zero.default_keymaps({buffer = bufnr})
+    end)
+
+    require("mason").setup({})
+    require("mason-lspconfig").setup({
+        ensure_installed = {},
+        handlers = {
+            lsp_zero.default_setup,
+            lua_ls = function()
+                local lua_opts = lsp_zero.nvim_lua_ls()
+                require("lspconfig").lua_ls.setup(lua_opts)
+            end,
+        }
+    })
 	--
 	-- TreeSitter
 	--
